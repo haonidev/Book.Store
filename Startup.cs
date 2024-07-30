ed6fe43c3aa21d693fcfbf78ad06ca524615106d
unknown <haoni.dev@outlook.com>
2024-07-30 07:55:44 -0300

    feat: Criando o Db Context e Entidades

---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿namespace Book.Store.Application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+                endpoints.MapGet("/health-check", async context =>
+                {
+                    await context.Response.WriteAsync("Healthy");
+                });
+            });
+        }
+    }
+}
